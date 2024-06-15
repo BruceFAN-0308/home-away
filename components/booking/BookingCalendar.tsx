7e90099311e50f3fc5a8b9c4b0c76668ab9364ac
Bruce <bruce.fan0308@gmail.com>
2024-06-15 16:26:38 -0400

    [UPDATE] change the type.ts to types.ts

--- components/booking/BookingCalendar.tsx
+++ components/booking/BookingCalendar.tsx
@@ -6,25 +6,63 @@ import {defaultSelected} from "@/utils/calendar";
 import {Calendar} from "@/components/ui/calendar";
 import {DateRange} from "react-day-picker";
 import {useProperty} from "@/utils/store";
+import {fetchBookingsByPropertyId} from "@/utils/actions";
 
-function BookingCalendar() {
+async function BookingCalendar() {
+    const state = useProperty((state) => state);
     const currentDate = new Date();
     const [range, setRange] = useState<DateRange | undefined>(defaultSelected);
+    const [bookings, setBookings] = useState<any[]>([]);
 
     const isDateDisabled = (date: Date): boolean => {
+        let isBooked = false;
+        //
+        if (bookings) {
+            const bookedDates = getBookedDates(bookings);
+            if (bookedDates.includes(date)) {
+                isBooked = true;
+            }
+        }
         // do not compare date directly, must use getDate() to get the day, because that will make today is not available
-        return date.getDate() < currentDate.getDate();
+        return date.getDate() < currentDate.getDate() || isBooked;
     }
 
     useEffect(() => {
         useProperty.setState({range});
     }, [range]);
 
+    useEffect(() => {
+        const fetchData = async () => {
+            const bookings = await fetchBookingsByPropertyId(state.propertyId);
+            setBookings(bookings);
+        };
+        fetchData();
+        console.log(bookings)
+    }, []);
+
     return (
         <Calendar mode="range" defaultMonth={currentDate} selected={range}
                   onSelect={setRange} className="mb-4" disabled={isDateDisabled}/>
     );
 }
+
+// a function to help me filter the bookings, to get the arrays, the arrays has all date is booked
+function getBookedDates(bookings: any[]): Date[] {
+    const bookedDates: Date[] = [];
+
+    bookings.forEach(booking => {
+        const currentDate = new Date(booking.checkIn);
+        const checkOutDate = new Date(booking.checkOut);
+
+        while (currentDate <= checkOutDate) {
+            bookedDates.push(new Date(currentDate));
+            currentDate.setDate(currentDate.getDate() + 1);
+        }
+    });
+
+    return bookedDates;
+}
+
 
 export default BookingCalendar;
 
