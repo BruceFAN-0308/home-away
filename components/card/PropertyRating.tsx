6784f475bbb044bffc522bebbd898628ea67404d
Bruce <bruce.fan0308@gmail.com>
2024-06-16 23:43:37 -0400

    [UPDATE] update reviews module

--- components/card/PropertyRating.tsx
+++ components/card/PropertyRating.tsx
@@ -1,18 +1,22 @@
-import React from 'react';
-import {FaStar} from "react-icons/fa";
+import { fetchPropertyRating } from '@/utils/actions';
+import { FaStar } from 'react-icons/fa';
 
-function PropertyRating({propertyId, inPage}: { propertyId: string, inPage: boolean }) {
-    const rating = 4.7
-    const count = 100
-
-    const className = `flex gap-1 items-center ${inPage ? 'text-md' : 'text-xs'}`
-    const countText = count > 1 ? 'reviews' : 'review'
-    const countValue = `(${count}) ${inPage ? countText : ''}`
+async function PropertyRating({
+                                  propertyId,
+                                  inPage,
+                              }: {
+    propertyId: string;
+    inPage: boolean;
+}) {
+    const { rating, count } = await fetchPropertyRating(propertyId);
+    if (count === 0) return null;
+    const className = `flex gap-1 items-center ${inPage ? 'text-md' : 'text-xs'}`;
+    const countText = count === 1 ? 'review' : 'reviews';
+    const countValue = `(${count}) ${inPage ? countText : ''}`;
     return (
         <span className={className}>
-        <FaStar className="w-3 h-3">
+      <FaStar className='w-3 h-3' />
             {rating} {countValue}
-        </FaStar>
     </span>
     );
 }
--- utils/actions.tsx
+++ utils/actions.tsx
@@ -676,3 +676,25 @@ export const fetchChartsData = async () => {
     }, [] as Array<{ date: string; count: number }>);
     return bookingsPerMonth;
 };
+
+
+export async function fetchPropertyRating(propertyId: string) {
+    const result = await db.review.groupBy({
+        by: ['propertyId'],
+        _avg: {
+            rating: true,
+        },
+        _count: {
+            rating: true,
+        },
+        where: {
+            propertyId,
+        },
+    });
+
+    // empty array if no reviews
+    return {
+        rating: result[0]?._avg.rating?.toFixed(1) ?? 0,
+        count: result[0]?._count.rating ?? 0,
+    };
+}
